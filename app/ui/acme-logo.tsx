14bdad6e454c6c4449d15a818c37cb460a32a28e
sangbinlee <sangbinlee9@gmail.com>
2025-01-21 07:42:07 +0900

    Update acme-logo.tsx

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -7,7 +7,7 @@ export default async function AcmeLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">sh shop</p>
+      <p className="text-[44px]">shop</p>
     </div>
   );
 }
